f7fcef5c3a60107d382a581614ddca96d1ebc8da
buckldav <runalldayandnight@gmail.com>
2018-08-25 22:18:50 -0600

    Outline w/comments

---
+++ notes.py
@@ -0,0 +1,38 @@
+"""
+Project:     Data Types Notes
+Author:      Mr. Buckley
+Last update: 8/25/2018
+Description: Goes over comments, int, float, str, and type casting  
+"""
+
+# *** COMMENTS ***
+# This is a comment (with a "#")
+# Comments are only for the user's eyes, the program doesn't read them.
+# Describe what sections of code do with a comment.
+"""
+This is a
+multiline comment
+"""
+
+# *** DATA TYPE: INTEGER ***
+# TODO: An integer number (no decimal)
+
+
+# *** DATA TYPE: FLOAT ***
+# TODO: A decimal number
+
+
+# *** DATA TYPE: STRING ***
+# TODO: A string of characters enclosed in quotes
+
+
+# *** TYPE CASTING ***
+# This converts one type to another
+
+# TODO: Cast float to int
+
+# TODO: Cast int to string
+
+# TODO: Cast number string to int
+
+# TODO: Input demo (str to float)
